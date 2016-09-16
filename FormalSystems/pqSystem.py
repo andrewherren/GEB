82955cbbabdbc6abbdae9f0ad209d30a56be3e67
Drew Herren <andrewscottherren@gmail.com>
2016-09-16 01:08:33 -0400

    added pq system code

--- FormalSystems/pqSystem.py
+++ FormalSystems/pqSystem.py
@@ -1,14 +1,31 @@
 """Encode Hofstadter's formal pq system"""
 import re
 
-class pq(object):
+
+class PQ(object):
     """format for working with string bound by pq rules"""
 
     def __init__(self, inputstring: str):
         """initialize string, validate it is an axiom"""
-        if re.search("-*p-+q-+", inputstring):
-            self.inputstring = inputstring
+        if re.search("-*p-*q-*", inputstring):
+            # split regex along p and q lines
+            resplit = re.split("p|q", inputstring)
+            # check is balance between "-" on either side of p and after q
+            if len(resplit[0]) + len(resplit[1]) == len(resplit[2]):
+                self.inputstring = inputstring
+            # do not initialize class if not valid under pq system
+            else:
+                raise ValueError
+        # do not initialize class if not valid under pq system
         else:
             raise ValueError
 
-    def
+    def axiom(self):
+        """update the string according to rule:
+
+        Suppose x, y, and z all stand for particular strings containing only
+        hyphens. And suppose that xpyqz is known to be a theorem. Then xpy-qz-
+        is a theorem.
+        """
+        resplit = re.split("p|q", self.inputstring)
+        self.inputstring = resplit[0] + 'p' + resplit[1] + '-q' + resplit[2] + '-'
